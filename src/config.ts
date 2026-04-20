9b14a956cdfda40e44468efceb367b0ef71dc3a8
Games55k <Games55k@163.com>
2026-04-20 17:49:50 +0800

    chore: 修改主标题

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Home",
-	subtitle: "碎碎念",
+	title: "灰太狼的狼堡",
+	subtitle: "",
 	lang: "zh_CN", // Language code, supports 'en' and 'zh_CN'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
